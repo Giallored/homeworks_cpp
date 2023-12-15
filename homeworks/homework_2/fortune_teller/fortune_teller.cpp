25bc8264b8ffa792b443da838472f37337a0cb15
Giallored <pacciarelli.1816493@studenti.uniroma1.it>
2023-12-15 11:47:42 +0100

    Added homework 2

---
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+
+string get_end(vector<string>& endings, string& name){
+    int n_end = endings.size();
+    int n_name = name.size();
+    int div = n_name % n_end;
+    string end = endings.at(div);
+    return end;
+}
+
+string get_adj(vector<string>& adjectives, string& name){
+    int n_adj = adjectives.size();
+    int n_name = name.size();
+    int div = n_name % n_adj;
+    string adj = adjectives.at(div);
+    return adj;
+}
+
+int main(){
+    string name,noun,adj_1,adj_2,season;
+
+    vector<string> adjectives;
+
+    map<string,string> nouns ={
+        {"spring", "STL guru"},
+        {"summer", "C++ expert"},
+        {"autumn", "coding beast"},
+        {"winter", "software design hero"}}; 
+
+    vector<string> endings = {"eats UB for breakfast",
+    "finds errors quicker than the compiler",
+    "is not afraid of C++ error messages"};
+
+    cout << "Welcome to the fortune teller program!\n";
+    
+    cout << "Please enter your name: "; 
+    cin >> name; 
+    
+    cout << "\nPlease enter the time of year when you were born:";
+    cout << "(pick from 'spring', 'summer', 'autumn', 'winter'): ";
+    cin >> season;
+
+    cout << "\nPlease enter an adjective: "; 
+    cin >> adj_1; 
+    cout << adj_1;
+    adjectives.push_back(adj_1);
+
+    cout << "\nPlease enter another adjective:"; 
+    cin >> adj_2;
+    cout << adj_2;
+    adjectives.push_back(adj_2);
+
+    auto adj = get_adj(adjectives,name);
+    auto end = get_end(endings,name);
+
+    //<NAME>, the <ADJECTIVE> <NOUN> that <ENDING>
+    printf("\n\n%s, the %s %s that %s\n", name.c_str(), adj.c_str(),
+                                     nouns.at(season).c_str(),end.c_str());
+    return 0;
+}
